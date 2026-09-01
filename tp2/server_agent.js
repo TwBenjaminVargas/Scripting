43f3057fb808b82122d5e315a0082697820c3d38
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2026-09-01 11:22:45 -0300

    comands input improves

--- tp2/server_agent.js
+++ tp2/server_agent.js
@@ -1,10 +1,14 @@
 import net from 'node:net';
+import { parseArgsStringToArgv } from 'string-argv'; // modulo especializado en parseo de argumentos en strings
 import { getosinfo } from './modules/osinfo.mjs';
 import { getwatches, watch } from './modules/watch.mjs';
 import { ps } from './modules/ps.mjs';
+import { oscmd, loadWhiteList } from './modules/oscmd.mjs';
 
 // Lectura de variables de entorno
 const port = process.env.PORT || 7777;
+const whitelist = process.env.WHITELIST || "wlist.json";
+loadWhiteList(whitelist);
 
 // Datos de cliente socket
 const clientData = socket => {return `${socket.remoteAddress}:${socket.remotePort}`;}
@@ -21,7 +25,7 @@ const serverResponse = (msj,command,err = false) =>
 
 
 // Obtencion de argumentos
-const parseCommand = (text) => {return text.trim().split(' ');}
+const parseCommand = (text) => {return parseArgsStringToArgv(text)}
 
 const server=net.createServer(
     socket => 
@@ -32,17 +36,22 @@ const server=net.createServer(
 
              socket.on('data', async data=>
                 {
+                    const datastr = data.toString()
                     try
                     {
-
-                        const command = parseCommand(data);
+                        const command = parseCommand(datastr);
+                        
                         switch(command[0])
                         {
                             case '':
                                 break;
+                            
+                            case 'oscmd':
+                                socket.write(serverResponse(await oscmd(command[1]),datastr));
+                                break;
 
                             case 'ps':
-                                socket.write(serverResponse(await ps(),data));
+                                socket.write(serverResponse(await ps(),datastr));
                                 break;
                     
                             case 'watch':
@@ -50,38 +59,38 @@ const server=net.createServer(
                                     throw new Error("Debes especificar al menos un path");
                                 const watchtoken = watch(command[1],Number(command[2]) || 60);
                                 console.log(`INFO - ${Date.now()}: ${clientData(socket)} inicio monitoreo de "${command[1]}", token: ${watchtoken}, timeout: ${command[2]}s`)
-                                socket.write(serverResponse(`Tu token de seguimiento es ${watchtoken}`,data));
+                                socket.write(serverResponse(`Tu token de seguimiento es ${watchtoken}`,datastr));
                                 break;
                                 
                             case 'getwatches':
                                 if(!command[1].trim())
                                     throw new Error("Debes especificar el token se seguimiento");
-                                socket.write(serverResponse(getwatches(command[1]),data));
+                                socket.write(serverResponse(getwatches(command[1]),datastr));
                                 break;
 
                             case 'getosinfo':
-                                socket.write(serverResponse(getosinfo(Number(command[1]) || 0),data));
+                                socket.write(serverResponse(getosinfo(Number(command[1]) || 0),datastr));
                                 break;
 
                             case 'quit':
                                 console.log(`${clientData(socket)} - Cerro sesión`);
-                                socket.write(serverResponse("Hasta luego!", data));
+                                socket.write(serverResponse("Hasta luego!", datastr));
                                 socket.end();
                                 break;
 
                             case 'help':
-                                socket.write(serverResponse(commandsDocumentation,data));
+                                socket.write(serverResponse(commandsDocumentation,datastr));
                                 break;
 
                             default:
-                                socket.write(serverResponse("No se encontró comando coincidente!",data,true));
+                                socket.write(serverResponse("No se encontró comando coincidente!",datastr,true));
                                 break;
                         }
                     }
                     catch(error)
                     {
                         console.log(`ERROR - ${Date.now()} :${clientData(socket)} ${error.message}`);
-                        socket.write(serverResponse(error.message,data,true));
+                        socket.write(serverResponse(error.message,datastr,true));
                     }
 
                 })
