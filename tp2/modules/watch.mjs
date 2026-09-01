b878d176c63d602c61bc0c3e81d53ded8bf9701d
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2026-09-01 08:40:29 -0300

    watch & getwatches added

---
+++ tp2/modules/watch.mjs
@@ -0,0 +1,51 @@
+import chokidar from 'chokidar'; //modulo de observacion de archivos
+import fs from 'node:fs';
+
+// mapa de archivos observados
+const watchMap = new Map();
+
+const watch = (path, timeout=60) =>
+    {
+        if (!fs.existsSync(path)) {
+            throw new Error(`La ruta "${path}" no existe en el sistema.`);
+        }
+
+        if(timeout>3600)
+            throw new Error("El maximo periodo de observacion es 1 hora");
+ 
+        const watcher = chokidar.watch(path,{ignoreInitial:true}); //ignoreInitial ignora elementos que ya estaban
+
+        const watchertoken = generateWatcherToken();
+        watchMap.set(watchertoken,[]);
+
+        watcher.on('all',(event, file)=>
+            {
+                watchMap.get(watchertoken).push({tipoEvento:event,archivo:file,tiempo:Date.now()})
+            }
+        )
+
+        setTimeout(() => 
+            {
+                watcher.close();
+            }, timeout * 1000);
+        
+        return watchertoken;
+    };
+
+const generateWatcherToken = () =>
+    {
+        const timestamp = Date.now();
+        // genera un número entero aleatorio entre 1000 y 99999
+        const randomNumber = Math.floor(1000 + Math.random() * 90000);
+        return `${timestamp}_${randomNumber}`;
+    }
+
+const getwatches = token =>
+    {
+        if (watchMap.has(token))
+            return watchMap.get(token);
+        else
+            return[];
+    }
+
+export {watch,getwatches};
--- tp2/server_agent.js
+++ tp2/server_agent.js
@@ -1,5 +1,6 @@
 import net from 'node:net';
 import { getosinfo } from './modules/osinfo.mjs';
+import { getwatches, watch } from './modules/watch.mjs';
 
 // Lectura de variables de entorno
 const port = process.env.PORT || 7777;
@@ -14,7 +15,7 @@ const commandsDocumentation ="getosinfo <n> --> informacion de memoria y cpu del
 // Respuesta estandar
 const serverResponse = (msj,command,err = false) =>
     {
-        return `\n${JSON.stringify({err: err, command: command, content: msj},null,2)}\n\n`
+        return `\n${JSON.stringify({command: command, content: msj,err: err},null,2)}\n\n`
     }
 
 
@@ -30,33 +31,47 @@ const server=net.createServer(
 
              socket.on('data', data=>
                 {
-                    const command = parseCommand(data);
-                    switch(command[0])
+                    try
                     {
-                        case '':
-                            break;
-                        case 'getosinfo':
-                            try
-                            {
-                                socket.write(serverResponse(getosinfo(Number(command[1]) || 0),data))
-                            }
-                            catch (error)
-                            {
-                                console.log(`${clientData(socket)} - Error: ${error.message}`);
-                                socket.write(serverResponse(error.message,data,true));
-                            }
-                            break;
-                        case 'quit':
-                            console.log(`${clientData(socket)} - Cerro sesión`);
-                            socket.write(serverResponse("Hasta luego!", data));
-                            socket.end();
-                            break;
-                        case 'help':
-                            socket.write(serverResponse(commandsDocumentation,data));
-                            break;
-                        default:
-                            socket.write(serverResponse("No se encontró comando coincidente!",data,true));
-                            break;
+
+                        const command = parseCommand(data);
+                        switch(command[0])
+                        {
+                            case '':
+                                break;
+                            
+                            case 'watch':
+                                if(!command[1].trim())
+                                    throw new Error("Debes especificar al menos un path");
+                                const watchtoken = watch(command[1],Number(command[2]) || 60);
+                                console.log(`INFO: ${clientData(socket)} inicio monitoreo de "${command[1]}", token: ${watchtoken}, timeout: ${command[2]}s`)
+                                socket.write(serverResponse(`Tu token de seguimiento es ${watchtoken}`,data));
+                                break;
+                            case 'getwatches':
+                                if(!command[1].trim())
+                                    throw new Error("Debes especificar el token se seguimiento");
+                                socket.write(serverResponse(getwatches(command[1]),data));
+                                break;
+                            case 'getosinfo':
+                                socket.write(serverResponse(getosinfo(Number(command[1]) || 0),data));
+                                break;
+                            case 'quit':
+                                console.log(`${clientData(socket)} - Cerro sesión`);
+                                socket.write(serverResponse("Hasta luego!", data));
+                                socket.end();
+                                break;
+                            case 'help':
+                                socket.write(serverResponse(commandsDocumentation,data));
+                                break;
+                            default:
+                                socket.write(serverResponse("No se encontró comando coincidente!",data,true));
+                                break;
+                        }
+                    }
+                    catch(error)
+                    {
+                        console.log(`${clientData(socket)} - Error: ${error.message}`);
+                        socket.write(serverResponse(error.message,data,true));
                     }
 
                 })
