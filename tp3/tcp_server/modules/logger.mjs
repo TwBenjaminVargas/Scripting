4ca67e038eea9f0938fd6af09d055e8d9a90a39e
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2026-09-15 15:47:53 -0300

    tp3 setup

---
+++ tp3/tcp_server/modules/logger.mjs
@@ -0,0 +1,47 @@
+
+// Colores para consola
+const COLORS = {
+    reset: "\x1b[0m",
+    bright: "\x1b[1m",
+    dim: "\x1b[2m",
+    red: "\x1b[31m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    magenta: "\x1b[35m",
+    cyan: "\x1b[36m"
+};
+
+const LOGLEVEL = Object.freeze(
+    {
+        ERROR: 0,
+        WARNING: 1,
+        INFO: 2,
+        DEBUG: 3,
+    })
+
+let currentLoggerLevel = LOGLEVEL.ERROR;
+
+const getFormattedDate = () =>
+{
+    const now = new Date();
+
+    // Formato local numérico (YYYY-MM-DD HH:mm:ss)
+    return new Intl.DateTimeFormat('sv-SE', {
+      dateStyle: 'short',
+      timeStyle: 'medium'
+    }).format(now);
+}
+const log = (label,color,msj) =>
+{
+    console.log (`[${color}${COLORS.bright}${label}${COLORS.reset}][${COLORS.cyan}[${getFormattedDate()}${COLORS.reset}]: ${msj}`);
+}
+
+const isLogAllowed = loglvl => {return loglvl <= currentLoggerLevel}
+
+const logE = (msj) => {if (isLogAllowed(LOGLEVEL.ERROR))log('ERROR',COLORS.red,msj);};
+const logI = (msj) => {if (isLogAllowed(LOGLEVEL.INFO)) log('INFO',COLORS.green,msj)};
+const logW = (msj) => {if (isLogAllowed(LOGLEVEL.WARNING)) log('WARN',COLORS.yellow,msj)};
+const logD = (msj) => {if (isLogAllowed(LOGLEVEL.DEBUG)) log('DEBUG',COLORS.magenta,msj)};
+const setLoggerLevel = (lvl) => {currentLoggerLevel = lvl};
+
+export {logE,logI,logW,logD, setLoggerLevel, LOGLEVEL};
---
+++ tp3/tcp_server/tcp_server.mjs
@@ -0,0 +1,87 @@
+import net from 'node:net';
+import { parseArgsStringToArgv } from 'string-argv'; // modulo especializado en parseo de argumentos en strings
+import { logD,logE,logI,logW, setLoggerLevel,LOGLEVEL } from './modules/logger.mjs';
+
+// Lectura de variables de entorno
+const port = process.env.PORT || 7777;
+const loggerLevel = process.env.LOGLEVEL || LOGLEVEL.INFO
+
+// establecer nivel de logs
+setLoggerLevel(loggerLevel)
+
+// Datos de cliente socket
+const clientData = socket => {return `${socket.remoteAddress}:${socket.remotePort}`;}
+
+
+// Documentacion de Agente
+const commandsDocumentation = [
+    "--- ESTRUCTURA DE RESPUESTA JSON ---",
+    "Todas las respuestas (salvo 'help') se envían con el siguiente formato:",
+    "{",
+    '  "command": "<comando enviado por el cliente>",',
+    '  "content": "<resultado obtenido o mensaje de error>",',
+    '  "err": true | false',
+    "}",
+    "",
+    "--- COMANDOS ---",
+    "snapshot                Toma una fotografia con la webcam",
+    "help                    Muestra este menú formateado en JSON",
+    "quit                    Cierra la conexión TCP con el servidor",
+    "help-cli                Muestra este menú en texto plano directo"
+].join('\n');
+
+// Respuesta estandar
+const serverResponse = (msj,command,err = false) =>
+    {
+        return `\n${JSON.stringify({command: command, content: msj,err: err},null,2)}\n\n`
+    }
+
+// Obtencion de argumentos
+const parseCommand = (text) => {return parseArgsStringToArgv(text)}
+
+const server=net.createServer(
+    socket => 
+        {
+            socket.setEncoding('utf8');
+            logI(`Nueva conexión ${clientData(socket)}`)
+            socket.write(`\nBenjamin Vargas - Server Agent 2026\n(Usa "help-cli" para consultar documentación)\n\n`);
+
+             socket.on('data', async data=>
+                {
+                    const datastr = data.toString().trim();
+                    try
+                    {
+                        const command = parseCommand(datastr);
+                        switch(command[0])
+                        {
+                            case '':
+                                break;
+                            case 'snapshot':
+                                break;
+
+                            case 'help':
+                                socket.write(serverResponse(commandsDocumentation,datastr));
+                                break;
+                            
+                            case 'quit':
+                                logI(`Cliente: ${clientData(socket)} cerro sesión`)
+                                socket.write(serverResponse("OK", datastr));
+                                socket.end();
+                                break;
+
+                            default:
+                                socket.write(serverResponse("No se encontró comando coincidente!",datastr,true));
+                                break;
+                        }
+                    }
+                    catch(error)
+                    {
+                        logE(`Cliente ${clientData(socket)} Mensaje: ${error.message}`)
+                        socket.write(serverResponse(error.message,datastr,true));
+                    }
+
+                })
+        });
+
+server.listen(port);
+logI(`Servidor corriendo en 127.0.0.1:${port}`)
